76fd6e920bab27e892e3297b6587eb0d4111001c
huangyichun <576305972@qq.com>
2017-06-03 10:55:21 +0800

    [樊蓉&黄义纯] 多次测试周围存活个数，包括边界测试

--- src/test/java/org/coach/tdd/template/LifeGameTest.java
+++ src/test/java/org/coach/tdd/template/LifeGameTest.java
@@ -10,18 +10,36 @@ import org.junit.Test;
 public class LifeGameTest {
 
     LifeGame lifeGame;
+    int[][] map;
     @Before
     public void initLifeGameParameter(){
         lifeGame = new LifeGame();
+        map = new int[8][8];
     }
 
     @Test
-    public void giveXYCountNeighborsLiveSize(){
-        int[][] map = new int[8][8];
+    public void giveSecondXYCountNeighborsLiveSize(){
         map[4][4] = 1;
         map[5][4] = 1;
         int count = lifeGame.countNeighborsLiveSize(map,5, 5);
         Assert.assertEquals(count, 2);
     }
+
+    @Test
+    public void giveOneXYCountNeighborsLiveSize(){
+        map[5][4] = 1;
+        int count = lifeGame.countNeighborsLiveSize(map,5, 5);
+        Assert.assertEquals(count, 1);
+    }
+
+    @Test
+    public void giveBorderXYCountNeighborsLiveSize(){
+        map[0][1] = 1;
+        map[1][0] = 1;
+        map[1][1] = 1;
+        int count = lifeGame.countNeighborsLiveSize(map,0, 0);
+        Assert.assertEquals(count, 3);
+    }
+
 
 }
